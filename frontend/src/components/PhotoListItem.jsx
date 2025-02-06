a0e7aebb8d6c2e6396ecb683e7eca80eb224c98a
Peter Heyboer <heyboer.peter@gmail.com>
2025-02-06 15:22:26 -0500

    adding styling to photolistitem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,13 +4,19 @@ import '../styles/PhotoListItem.scss';
 
 const PhotoListItem = ({ photo }) => {
   return (
-    <div key={photo.id}>
-      <img src={photo.imageSource} />
-      <div>
-        <img src={photo.profile} />
-        <div>
-          <p>{photo.username}</p>
-          <p>{photo.location.city}, {photo.location.country}</p>
+    <div key={photo.id} className="photo-list__item">
+      <img className="photo-list__image" src={photo.imageSource} alt="Photo" />
+      <div className="photo-list__details">
+        <img
+          className="photo-list__profile"
+          src={photo.profile}
+          alt="Profile"
+        />
+        <div className="photo-list__info">
+          <p className="photo-list__username">{photo.username}</p>
+          <p className="photo-list__location">
+            {photo.location.city}, {photo.location.country}
+          </p>
         </div>
       </div>
     </div>
