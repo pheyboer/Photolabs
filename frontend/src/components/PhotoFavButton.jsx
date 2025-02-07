0d9904cdcbabbce34293af181a34b2115cd0f133
Peter Heyboer <heyboer.peter@gmail.com>
2025-02-06 19:21:04 -0500

    updated photofavbutton.jsx to use props from photolistitem.jsx

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,17 +4,16 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 //using function expression for component eslint
-const PhotoFavButton = () => {
-  //state for favouriting picture
-  const [isFavourite, setIsFavourite] = useState(false);
-
+const PhotoFavButton = ({ selected, onFavouriteToggle }) => {
   //toggle state
-  const toggleFavourite = useCallback(() => {
-    setIsFavourite((prevState) => !prevState);
-  }, []);
+  const handleClick = useCallback(() => {
+    if (onFavouriteToggle) {
+      onFavouriteToggle();
+    }
+  }, [onFavouriteToggle]);
 
   return (
-    <div className="photo-list__fav-icon" onClick={toggleFavourite}>
+    <div className="photo-list__fav-icon" onClick={handleClick}>
       <div
         className={`photo-list__fav-icon-svg ${
           isFavourite ? 'favourited' : ''
