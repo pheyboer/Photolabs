8ad89ff9dcdff222b3ab8e20b3fd2b42bf0eb5b2
Peter Heyboer <heyboer.peter@gmail.com>
2025-02-10 14:11:09 -0500

    implementing close button for photodetailsmodal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -3,12 +3,20 @@ import React from 'react';
 import '../styles/PhotoDetailsModal.scss';
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = () => {
+const PhotoDetailsModal = ({ photo, closeModal }) => {
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
+      <button
+        className="photo-details-modal__close-button"
+        onClick={closeModal}
+      >
         <img src={closeSymbol} alt="close symbol" />
       </button>
+      {/* <img
+        className="photo-details-modal__image"
+        src={photo.urls.regular}
+        alt="Selected"
+      /> */}
     </div>
   );
 };
